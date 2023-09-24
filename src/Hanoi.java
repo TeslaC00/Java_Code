a9757a802e5dd3e0535518731fa1985e3a8b88b7
TeslaC00 <vikaskumar16122002@gmail.com>
2023-09-24 20:03:05 +0530

    spelling check

--- src/Hanoi.java
+++ src/Hanoi.java
@@ -6,10 +6,10 @@ public class Hanoi {
     public static void main(String[] args) {
         ListStack<Integer> src = new ListStack<>(), help = new ListStack<>(), dst = new ListStack<>();
         src.addAll(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
-        System.out.println("Source before transfering: ");
+        System.out.println("Source before transferring: ");
         src.display();
         hanoi(src.size(), src, help, dst);
-        System.out.println("Destination after transfering:");
+        System.out.println("Destination after transferring:");
         dst.display();
     }
 
