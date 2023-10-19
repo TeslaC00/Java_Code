30656ce6ec50affb10f201b4e6174d7f354f6a6e
TeslaC00 <vikaskumar16122002@gmail.com>
2023-10-19 22:44:43 +0530

    added quickSort and mergeSort

--- src/Sorter.java
+++ src/Sorter.java
@@ -36,4 +36,66 @@ public class Sorter {
             arr[j + 1] = x;
         }
     }
+
+    public static void mergeSort(int[] arr) {
+        divide(arr, 0, arr.length - 1);
+    }
+
+    private static void divide(int[] arr, int start, int end) {
+        if (start >= end)
+            return;
+        int mid = start + (end - start) / 2;
+        divide(arr, start, mid);
+        divide(arr, mid + 1, end);
+        merge(arr, start, mid, end);
+    }
+
+    private static void merge(int[] arr, int start, int mid, int end) {
+        int sorted[] = new int[end - start + 1];
+        int left = start, right = mid + 1, i = 0;
+        while (left <= mid && right <= end) {
+            if (arr[left] < arr[right]) {
+                sorted[i++] = arr[left++];
+            } else {
+                sorted[i++] = arr[right++];
+            }
+        }
+        while (left <= mid) {
+            sorted[i++] = arr[left++];
+        }
+        while (right <= end) {
+            sorted[i++] = arr[right++];
+        }
+        for (int j = 0; j < i; j++) {
+            arr[start++] = sorted[j];
+        }
+
+    }
+
+    public static void quickSort(int[] arr) {
+        qSort(arr, 0, arr.length - 1);
+    }
+
+    private static void qSort(int[] arr, int low, int high) {
+        if (low >= high)
+            return;
+        int pivot = partition(arr, low, high);
+        qSort(arr, low, pivot - 1);
+        qSort(arr, pivot + 1, high);
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int left = low - 1;
+        for (int right = low; right < high; right++) {
+            if (arr[right] < pivot) {
+                int temp = arr[right];
+                arr[right] = arr[++left];
+                arr[left] = temp;
+            }
+        }
+        arr[high] = arr[++left];
+        arr[left] = pivot;
+        return left;
+    }
 }
--- src/SorterTest.java
+++ src/SorterTest.java
@@ -1,7 +1,12 @@
 public class SorterTest {
     public static void main(String[] args) {
         int[] numbers = { 6, 21, 55, 12, 5, 7, 1 };
-        Sorter.insertionSort(numbers);
+        System.out.println("Array before sorting");
+        for(int i: numbers) {
+            System.out.print(i+" ");
+        }
+        System.out.println("\nArray after sorting");
+        Sorter.quickSort(numbers);
         for (int i : numbers) {
             System.out.print(i + " ");
         }
